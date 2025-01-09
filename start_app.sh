abe627a2ef86ef827b1df4a5f21519d1e6ac1ffa
Sandhj <135236046+Sandhj@users.noreply.github.com>
2025-01-09 18:38:35 +0800

    Update start_app.sh

--- start_app.sh
+++ start_app.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 # Path absolut ke virtual environment
-VENV_PATH="/root/user/w/myenv"
-
+VENV_PATH="/root/user/restore-web/myenv"
 # Path absolut ke aplikasi
-APP_PATH="/root/myproject/app.py"
-
+APP_PATH="/root/user/restore-web/app.py"
 # Menjalankan aplikasi menggunakan Python dari virtual environment dan mengalihkan output
-$VENV_PATH/bin/python $APP_PATH > /root/myproject/app.log 2>&1 &
+$VENV_PATH/bin/python $APP_PATH > /root/user/restore-web/app.log 2>&1 &
+
+echo -e " - Silahkan Lakukan Restore Melalui Browser -"
+echo -e "Akses : http://<IP_VPS>:5001"
