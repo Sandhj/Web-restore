63848c34af45c8e5cce8f5151c3f5a99c9befb16
Sandhj <135236046+Sandhj@users.noreply.github.com>
2025-01-09 17:33:19 +0800

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,16 @@
+#Uodate Package
+sudo apt update
+#Instal Paket Virtual Envi
+sudo apt install python3-venv
+#Buat Lokasi Projek
+mkdir -p /root/user/restore-web/
+#Masuk Ke Lokasi Projek
+cd /root/user/restore-web/
+#buat python Env
+python3 -m venv myenv
+#Aktifkan Env 
+source myenv/bin/activate
+#Install Modul Yang Diinginkan
+pip install flask
+
+
